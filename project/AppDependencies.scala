1528a026310627afaea4be490d074062a29f24d0
Lucio Biondi <lucio.biondi@digital.hmrc.gov.uk>
2020-08-25 11:15:54 +0100

    [BS=121][LB] update app.routes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,9 +10,9 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-27" % "2.24.0"  % Test,
-    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.35.10" % "test, it",
+    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.36.8" % "test, it",
     "com.typesafe.play"      %% "play-test"              % current   % Test,
-    "org.scalatest"          %% "scalatest"              % "3.1.2"   % Test,
+    "org.scalatest"          %% "scalatest"              % "3.2.0"   % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "4.0.3"   % "test, it"
   )
 }
