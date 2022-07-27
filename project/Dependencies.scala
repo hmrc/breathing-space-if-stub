4f936872f0e46f1a17a8256cf5a2ec7607627249
Kurt Logan <4375059+kurtlogan@users.noreply.github.com>
2022-07-27 08:57:44 +0100

    upgrade dependencies (#37)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,16 +4,16 @@ import sbt._
 object Dependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"  % "5.7.0",
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"  % "6.4.0",
     "com.beachape"                 %% "enumeratum"                 % "1.7.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.12.4"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.3"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.7.0"  % "it",
-    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.36.8" % "it",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "6.4.0"  % "it",
+    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.62.2" % "it",
     "com.typesafe.play"      %% "play-test"              % current  % "it",
-    "org.scalatest"          %% "scalatest"              % "3.2.9"  % "it",
+    "org.scalatest"          %% "scalatest"              % "3.2.12"  % "it",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"  % "it"
   )
 }
