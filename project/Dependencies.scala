140eae5e3dc74601ae7917bd9b9c4eddc6a28ff6
Kurt Logan <4375059+kurtlogan@users.noreply.github.com>
2023-04-03 15:21:14 +0100

    BS-666 | address pr comments (#39)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,14 +3,16 @@ import sbt._
 
 object Dependencies {
 
+  val bootstrapVersion = "6.4.0"
+
   val compile = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"  % "6.4.0",
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"  % bootstrapVersion,
     "com.beachape"                 %% "enumeratum"                 % "1.7.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.3"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "6.4.0"  % "it",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion % "it",
     "com.vladsch.flexmark"   %  "flexmark-all"           % "0.62.2" % "it",
     "com.typesafe.play"      %% "play-test"              % current  % "it",
     "org.scalatest"          %% "scalatest"              % "3.2.12"  % "it",
